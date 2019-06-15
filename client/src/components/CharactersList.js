f1bcc0a94fb137f5bb2cd5f0469bf0fbc9d6ec59
Cris Noel <crisnoel12@gmail.com>
2019-06-15 11:44:50 -0700

    changed characterslist component to functional

--- client/src/components/CharactersList.js
+++ client/src/components/CharactersList.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import CharacterListItem from './CharacterListItem';
 
-class CharactersList extends Component {
-  render() {
-    return (
-      <InfiniteScroll
-        dataLength={this.props.characters.length}
-        next={this.props.onLoadMore}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-      >
-        {this.props.characters.map((character) => (
-          <CharacterListItem key={character.id} character={character} />
-        ))}
-      </InfiniteScroll>
-    );
-  }
+const CharactersList = (props) => {
+  return (
+    <InfiniteScroll
+      dataLength={props.characters.length}
+      next={props.onLoadMore}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
+      {props.characters.map((character) => (
+        <CharacterListItem key={character.id} character={character} />
+      ))}
+    </InfiniteScroll>
+  )
 }
 
 export default CharactersList;
+
---
+++ public/build/precache-manifest.80b3025a9f0acfe1b41e16c1e28dc3d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8b0a08adfdd77aec31c3e4a6736856",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b57aa90e290395acb72",
+    "url": "/static/css/main.caf01f46.chunk.css"
+  },
+  {
+    "revision": "f361a4a54c1c86537a26",
+    "url": "/static/js/2.3adfbbba.chunk.js"
+  },
+  {
+    "revision": "9b57aa90e290395acb72",
+    "url": "/static/js/main.e82a199f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ae20c7c2d2a4d9838e0f37c131ad9b1",
+    "url": "/static/media/logo.7ae20c7c.jpg"
+  },
+  {
+    "revision": "99da7b2f50a0aeec33e3f43acc4cf6db",
+    "url": "/static/media/marvel.99da7b2f.jpg"
+  }
+]);
